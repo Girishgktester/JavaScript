0747a0553ff7c339c7770fce3e6fd60d53165f2f
Girishgktester <girishgk.tester@gmail.com>
2025-02-10 18:17:41 +0530

    switchcase

---
+++ JSPractice/switch.js
@@ -0,0 +1,15 @@
+let browser = "chrome";  // switch case in JS same as Java
+
+switch (browser) {
+  case "chrome":
+    console.log("The browser is Chrome");
+    break;
+  case "firefox":
+    console.log("The browser is Firefox");
+    break;
+  case "safari":
+    console.log("The browser is Safari");
+    break;
+  default:
+    console.log("The browser is not Chrome, Firefox, or Safari");
+}
