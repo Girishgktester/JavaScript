14a95cacbe68bc3826d73263cbf0fe36fb5e5417
Girishgktester <girishgk.tester@gmail.com>
2025-02-17 13:38:49 +0530

    Arrow

---
+++ JSPractice/ArrowFunction.js
@@ -0,0 +1,38 @@
+// arrow funtion doesnt have any name
+// arrow function doesnt have any return keyword
+// arrow function doesnt have any curly braces
+// arrow function doesnt have any parameter brackets
+// arrow function doesnt have any function keyword
+// arrow function is used in ES6
+// arrow function is used in modern JS
+// arrow function is used in async programming
+// arrow function is used in functional programming
+
+const squre = (n) => n * n;  // no prameter 
+console.log(squre(4));  // Output: 16
+
+// one parameter 
+const mesg = () => 'Hello';  // Output: Hello
+console.log(mesg());  // Output: Hello
+
+// multiple parameters
+const add = (a, b) => a + b;
+
+
+console.log(add(2, 3));  // Output: 5
+
+const getName = person => `${person.name} ${person.age}`
+
+
+const person = {
+    name: 'John',
+    age: 30,
+}
+
+console.log(getName(person));  // Output: John 30
+
+
+
+
+
+
