cd72fb7fffcaa0d6ba698155d4bd33614c5a9cac
Girishgktester <girishgk.tester@gmail.com>
2025-02-18 12:45:08 +0530

    Classes

---
+++ JSPractice/class.js
@@ -0,0 +1,28 @@
+class car{
+
+    constructor(name, price, model){
+        this.name = name;
+        this.price = price;
+        this.model = model;
+    }
+
+
+    refuel(){
+        console.log(this.name + ' Refueling');
+    }
+}
+
+
+const car1 = new car('Audi', 50000, 'A6');
+console.log(car1.name);
+console.log(car1.price);
+console.log(car1.model);
+car1.refuel();
+
+const  c2 = new car('BMW', 60000, 'X5');
+console.log(c2.name);
+console.log(c2.price);
+console.log(c2.model);
+c2.refuel();
+
+
