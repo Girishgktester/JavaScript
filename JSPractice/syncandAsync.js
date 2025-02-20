f6730fad42376b037b8269f44d6eb8bd81a990c2
Girishgktester <girishgk.tester@gmail.com>
2025-02-20 18:19:53 +0530

    Sync and Async

---
+++ JSPractice/syncandAsync.js
@@ -0,0 +1,25 @@
+console.log('Task 1: Start');
+
+function performTask() {
+    // Simulating a time-consuming task
+    const start = Date.now();
+    while (Date.now() - start < 2000) {
+        // Blocking loop for 2 seconds
+    }
+    console.log('Task 2: Performing Task');
+}
+
+performTask();
+console.log('Task 3: End');
+
+
+console.log('Task 1: Start');
+
+function performTaskAsync() {
+    setTimeout(() => {
+        console.log('Task 2: Performing Task');
+    }, 2000); // Simulating a delay of 2 seconds
+}
+
+performTaskAsync();
+console.log('Task 3: End');
