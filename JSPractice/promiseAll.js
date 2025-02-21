9afd5fcaa1c0955ecb2b574985a831607dbea380
Girishgktester <girishgk.tester@gmail.com>
2025-02-21 15:13:30 +0530

    promiseAll

---
+++ JSPractice/promiseAll.js
@@ -0,0 +1,47 @@
+// case1 
+// Promise.all([promise1, promise2, promise3]).then((values) => {
+//     console.log(values);
+// });
+
+
+// case1 
+
+const function1 = () =>{
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('Function 1');
+        }, 2000);
+    });
+}
+
+// case 2
+
+const function2 = () =>{
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('Function 2');
+        }, 2000);
+    });
+}
+
+// case3 
+const function3 = () =>{
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('Function 3');
+        }, 2000);
+    });
+}
+
+Promise.all([
+    function1(),
+     function2(),
+      function3()])
+      .then((dataArray ) => {
+            console.log(dataArray);
+})
+
+.catch((error) => {
+    console.log('Error: ' + error);
+});
+
